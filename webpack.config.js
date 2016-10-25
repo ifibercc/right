24fa373d76787154bcd7b904b95960238ac1f08f
Lemoo <ifibercc@gmail.com>
2016-10-25 17:19:47 +0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+    entry: APP_PATH,
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        process: true
+    },
+    resolve: {
+        extension: ['', '.js', '.jsx']
+    }
+};
