0daf8d5f63a3aad5e84882592b26cd64d26a296c
Lemoo <ifibercc@gmail.com>
2016-10-29 16:03:29 +0800

    demo

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import { DatePicker, Button } from 'antd'
+import moment from 'moment'
 
+const format = 'YYYY/MM/DD'
 class App extends Component {
     render() {
         return (
-          <div>Hello World!</div>
+          <div><DatePicker defaultValue={moment('2015/01/01', format)} />
+              <br />
+              <Button type="primary">Test</Button>
+
+          </div>
         )
     }
 }
